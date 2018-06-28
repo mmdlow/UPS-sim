f9d2643610c52e2e7bdcf5b62996190334df484a
Donald <d-lee@comp.nus.edu.sg>
2018-06-28 21:35:33 +0800

    Add damped camera movement

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class cameraController : MonoBehaviour {
-
-	public GameObject player;
-	private Vector3 offset;
-	// Use this for initialization
-	void Start () 
-	{
-		offset = transform.position - player.transform.position;
-	}
 	
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+
 	// Update is called once per frame
-	void LateUpdate () 
+	void Update () 
 	{
-		transform.position = player.transform.position + offset;
+		if (target)
+		{
+			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+	
 	}
 }
