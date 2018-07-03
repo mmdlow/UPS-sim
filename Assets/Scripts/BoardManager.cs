663d4af5d1111d25cff1612f1ef2e08d66346420
Donald <d-lee@comp.nus.edu.sg>
2018-07-04 01:48:00 +0800

    WIP refactor boardManager for message passing
     - refactor a bunch of stuff to use new methods
     - move waypoint init from bm into waypoint
     - Breaks waypoints!

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -8,16 +8,14 @@ using Random = UnityEngine.Random;
 /*
 	BoardManager manages instances of Unity scenes i.e. levels. For every new 
 	scene there should be a new instance of boardManager.
+
+	Apart from GameManager, BoardManager, all other GameObjects should only 
+	"talk" to their own static classes. GameObjects shall *not* reference static
+	methods of other classes.
  */
 public class BoardManager : MonoBehaviour {
 
 	public static BoardManager instance = null;
-	public GameObject ground;
-	public GameObject waypoint;
-	private GameObject currentWaypoint;
-	private List<Vector3Int> reachablePositions = new List<Vector3Int>();
-	public int waypointCount = 10;
-	private int currWaypointCount = 0;
 
 	void Awake() {
 		if (instance == null) {
@@ -25,43 +23,24 @@ public class BoardManager : MonoBehaviour {
 		} else if (instance != this) {
 			Destroy(gameObject); // enforce singleton pattern wrt BoardManager
 		}
-
-		ground = GameObject.Find("Ground");
-	}
-	void InitReachablePositions() {
-		reachablePositions.Clear();
-		Tilemap groundTilemap = ground.transform.Find("Tilemap-ground").gameObject.GetComponent(typeof(Tilemap)) as Tilemap;
-        // TileBase[] groundTilesArray = groundTilemap.GetTilesBlock(groundTilemap.cellBounds);
-		for (int x=groundTilemap.origin.x; x<groundTilemap.size.x; x++) {
-			for (int y=groundTilemap.origin.y; y<groundTilemap.size.y; y++) {
-				TileBase tile = groundTilemap.GetTile(new Vector3Int(x, y, 0));
-				if (tile != null && tile.name.StartsWith("pavement")) {
-					reachablePositions.Add(new Vector3Int(x, y, 0));
-				}
-			}
-		}
 	}
 
-	void LayoutWaypointAtRandom() {
-		Vector3 randomPosition = reachablePositions[Random.Range (0, reachablePositions.Count)];
-		currentWaypoint = Instantiate(waypoint, randomPosition, Quaternion.identity);
-	}
-	public GameObject GetCurrentWaypoint() {
-		return currentWaypoint;
-	}
-
-	public void LoadNextWaypoint() {
-		if (currWaypointCount <= waypointCount) {
-			LayoutWaypointAtRandom();
-			currWaypointCount++;
-		}
-	}
 	public void SetupLevel() {
-		// Reset reachablePositions
-		InitReachablePositions();
+		Waypoint.Init();
+	}
+
+	// Use message passing to talk to BM. Check your spelling!
+	public void Send(String message) {
+		switch(message) {
+			case "LoadNextWaypoint":
+				Waypoint.LoadNextWaypoint();
+				break;
+			default:
+				Debug.LogError("Unknown message! Check your spelling.");
+				break;
+		}
 	}
 	public void StartLevel() {
-		LoadNextWaypoint();
-		Debug.Log("Hello from BM!");
+		this.Send("LoadNextWaypoint");  // Try to avoid directly calling Waypoint.LoadNextWaypoint()
 	}
 }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour {
 
+    private static GameObject ground;
+	private static List<Vector3Int> reachablePositions = new List<Vector3Int>();
 	private BoardManager boardManager;
-	//public GameObject gameManager;
-	// Use this for initialization
+
+	public static void Init() {
+		ground = GameObject.Find("Ground");
+	}
+
+	public static void InitReachablePositions() {
+		reachablePositions.Clear();
+		Tilemap groundTilemap = ground.transform.Find("Tilemap-ground").gameObject.GetComponent(typeof(Tilemap)) as Tilemap;
+        // TileBase[] groundTilesArray = groundTilemap.GetTilesBlock(groundTilemap.cellBounds);
+		for (int x=groundTilemap.origin.x; x<groundTilemap.size.x; x++) {
+			for (int y=groundTilemap.origin.y; y<groundTilemap.size.y; y++) {
+				TileBase tile = groundTilemap.GetTile(new Vector3Int(x, y, 0));
+				if (tile != null && tile.name.StartsWith("pavement")) {
+					reachablePositions.Add(new Vector3Int(x, y, 0));
+				}
+			}
+		}
+	}
+
+	public static void LayoutWaypointAtRandom() {
+		Vector3 randomPosition = reachablePositions[Random.Range (0, reachablePositions.Count)];
+		currentWaypoint = Instantiate(waypoint, randomPosition, Quaternion.identity);
+	}
+
+	public static void LoadNextWaypoint() {
+		if (currWaypointCount <= waypointCount) {
+			LayoutWaypointAtRandom();
+			currWaypointCount++;
+		}
+	}
 	void Awake () {
 		boardManager = BoardManager.instance;
 	}
