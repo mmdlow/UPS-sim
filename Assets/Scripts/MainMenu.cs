563ac6645db5322be0a25c972b4601e2cc453cee
mmdlow <mmdlow@gmail.com>
2018-07-10 01:02:37 +0800

    Create main menu scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame() {
+		// Load next level in queue
+		Debug.Log("Starting game");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void GoToSettings() {
+		Debug.Log("Opening Settings menu");
+	}
+
+	public void GoToCredits() {
+		Debug.Log("Opening Credits");
+	}
+
+	public void QuitGame() {
+		Debug.Log("Quitting game");
+		Application.Quit();
+	}
+}
