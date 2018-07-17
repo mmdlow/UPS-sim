1160ce8f23370e8bd97284c0884c72282dbe2235
Donald <d-lee@comp.nus.edu.sg>
2018-07-18 00:29:07 +0800

    Fix add/remove item, forgot to add to list

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -63,10 +63,12 @@ public class ItemManager : MonoBehaviour {
 	}
 
 	public void AddItem(GameObject item) {
+		items.Add(item);
 		onItemAdd(item);
 	}
 
 	public void RemoveItem(GameObject item) {
+		items.Remove(item);
 		onItemRemove(item);
 	}
 }
