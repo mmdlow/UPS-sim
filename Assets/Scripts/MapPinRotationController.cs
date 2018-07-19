84efbeece1dca513ed9b5b6367cac5a41bfc92a1
mmdlow <mmdlow@gmail.com>
2018-07-19 22:50:23 +0800

    Add more grass and soil tiles

---
+++ Assets/Scripts/MapPinRotationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPinRotationController : MonoBehaviour {
+
+	GameObject worldmapCamera;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		worldmapCamera = GameObject.Find("Worldmap Camera");
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (name.Equals("Worldmap Truck Pin")) {
+			transform.rotation = worldmapCamera.transform.rotation;
+			return;
+		}
+		transform.rotation = player.transform.rotation;
+	}
+}
