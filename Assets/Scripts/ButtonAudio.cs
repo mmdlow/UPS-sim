35db85bd2cdec3ba14e7df34a4c512cdb2027837
mmdlow <mmdlow@gmail.com>
2018-07-29 17:57:32 +0800

    Add hover/click sfx to buttons

---
+++ Assets/Scripts/ButtonAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudio : MonoBehaviour {
+
+	public AudioClip hoverSound;
+	public AudioClip clickSound;
+
+	public void PlayClickSound() {
+		SoundManager.instance.PlaySingle(clickSound);
+	}
+
+	public void PlayHoverSound() {
+		SoundManager.instance.PlaySingle(hoverSound);
+	}
+}
