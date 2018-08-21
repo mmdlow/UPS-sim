690fae4814074074e0755833b75f61e04e25e680
mmdlow <mmdlow@gmail.com>
2018-08-21 18:52:27 +0800

    Enable main menu button selection with keyboard

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,13 +14,31 @@ public class MainMenu : MonoBehaviour {
 		menuButtons = GetComponentsInChildren<Button>();
 	}
 
+	void OnEnable() {
+		StartCoroutine("WaitForPointer");
+		menuButtons[buttonIndex].OnSelect(null);
+	}
+
+	IEnumerator WaitForPointer() {
+		yield return null;
+		pointerAnim.SetInteger("selectOption", buttonIndex);
+	}
+
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
 			if (buttonIndex <= 0) return;
+			menuButtons[buttonIndex].OnDeselect(null);
 			pointerAnim.SetInteger("selectOption", --buttonIndex);
+			menuButtons[buttonIndex].Select();
+			menuButtons[buttonIndex].OnSelect(null);
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
 			if (buttonIndex >= 3) return;
+			menuButtons[buttonIndex].OnDeselect(null);
 			pointerAnim.SetInteger("selectOption", ++buttonIndex);
+			menuButtons[buttonIndex].Select();
+			menuButtons[buttonIndex].OnSelect(null);
+		} else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)) {
+			menuButtons[buttonIndex].onClick.Invoke();
 		}
 	}
 
